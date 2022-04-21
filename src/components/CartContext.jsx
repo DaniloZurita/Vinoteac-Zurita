1e8d094c8fbbfe5ce39bec3910062d9e22bc55b3
Danilo Zurita <danizurita4@gmail.com>
2022-04-21 11:40:44 -0300

    cartlloro}

---
+++ src/components/CartContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useState } from "react";
+
+export const CartContext = createContext();
+
+const CartContextProvider = ({children}) => {
+
+     const [cartList, setCartList] = useState([]);
+
+     const cantidadTotal = () =>
+
+        cartList.reduce((acum, element) => (acum += element.quantity), 0);
+
+     const addToCart = (item, quantity) => {
+         let previo = cartList.find(element => element.id === item.id);
+         if(previo === undefined) {
+            setCartList([
+                ...cartList, 
+                item
+            ])
+        } else {
+            previo.quantity += quantity;
+            }   
+     }
+
+     const clear = () => {
+         setCartList([])
+     }   
+
+     const borrarProducto = (idRemantente) => {
+         let remanenteCarro = cartList.filter(element => element.id !== idRemantente);
+         setCartList(remanenteCarro);
+     }
+
+    return(
+        <CartContext.Provider value={{cartList, addToCart, clear, borrarProducto, cantidadTotal}}>
+            {children}
+        </CartContext.Provider>
+    ); 
+}
+
+export default CartContextProvider;
--- src/components/CheckOut.jsx
+++ src/components/CheckOut.jsx
@@ -1,11 +1,10 @@
-import { Button } from 'bootstrap'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 const CheckOut = () => {
     return(
         <>
-           <Link to='/cart'><button className='btn-primary'>Finalizar Compra</button></Link>
+           <Link to='/cart'><button className='btn-primary'>Cargar en mi carrito!</button></Link>
         </>
     );
 }
